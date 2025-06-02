d2c4f99e51ad5216b2abf0b0d35f421742217dda
ChinmayPatil <patilchinmay62@gmail.com>
2025-06-01 22:54:30 -0400

    Font size

--- src/App.tsx
+++ src/App.tsx
@@ -232,7 +232,7 @@ function App() {
 
       <section id="why-convoo" className="scroll-section min-h-screen bg-[#121212] flex items-center justify-center py-20">
         <div className="container mx-auto px-4 md:px-6">
-          <h2 className="text-white mb-10 md:mb-14 text-center text-3xl md:text-4xl lg:text-5xl font-poppins font-light">
+          <h2 className="text-white mb-10 md:mb-14 text-center text-4xl md:text-4xl lg:text-5xl font-poppins font-light">
             Why Convoo?
           </h2>
           
